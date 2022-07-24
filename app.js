2db5fb4162072bce6df94bd5f82210cfa5da71b8
Must <Mumuste.mend@gmail.com>
2022-07-24 18:25:27 +0100

    fixing bugs to  launch the server

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const dotEnv = require('./config/config');
 const helmet = require('helmet'); //for security (frame cross etc...)
 const morgan = require('./config/morgan'); //for logs
 const xss = require('xss-clean');
-const i18nextMiddleware = require('./config/i18n'); 
-const {errorHandler,errorConverter}= require('./middlewares/error');
+// const i18nextMiddleware = require('./config/i18n');
+const { errorHandler, errorConverter } = require('./middlewares/error');
 const compression = require('./middlewares/compressionMiddleware');
 
 const app = express();
@@ -13,10 +13,10 @@ const app = express();
 
 
 //for multiple languages support
-app.use(i18nextMiddleware);
+// app.use(i18nextMiddleware);
 
 //morgan to logs details about triggred endpoints
-app.use(morgan.successHandler,morgan.errorHandler);
+app.use(morgan.successHandler, morgan.errorHandler);
 
 //For security
 app.use(helmet());
@@ -36,7 +36,7 @@ app.use(xss());
 app.use(compression);
 
 //to convert and handle Errors
-app.use(errorConverter,errorHandler);
+app.use(errorConverter, errorHandler);
 
 
 
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const catchAsync = require('./utils/catchAsync');
 
 
 
-app.get('/data',catchAsync(async(req,res)=>{
+app.get('/data', catchAsync(async (req, res) => {
 
-        throw new ApiError(400,'something wrong');
-        res.send("oke");
-        // throw new ApiError(httpStatus.BAD_REQUEST,"error");
-    
-        
+    throw new ApiError(400, 'something wrong');
+    res.send("oke");
+    // throw new ApiError(httpStatus.BAD_REQUEST,"error");
+
+
 }))
 
 
@@ -23,9 +23,9 @@ app.get('/data',catchAsync(async(req,res)=>{
 // app.get()
 
 
-app.listen(process.env.PORT | 3001 ,()=>{
-    console.log("server started on port",process.env.PORT);
-    console.log("mode:",process.env.NODE_ENV);
-    console.log("state",process.env.STATE) 
+app.listen(process.env.PORT | 3001, () => {
+    console.log("server started on port", process.env.PORT);
+    console.log("mode:", process.env.NODE_ENV);
+    console.log("state", process.env.STATE)
 
 })
